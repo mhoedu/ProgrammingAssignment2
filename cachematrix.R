81e3554452431af6c115c1f2f71b2cf42abc257b
Michael Ho <mhoedu@gmail.com>
2015-11-22 17:46:05 -0600

    Implement makeCacheMatrix and cacheSolve to fullfill requirements of programming assignment 2
    makeCacheMatrix : take an input matrix and define a special matrix that can
     cache it inverse
    cacheSolve: take an input "makeCacheMatrix" matrix x and determine if
     the inverse matrix already caculate or not. If it is define, getting it from
     cach else use solve to define the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,64 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix: defines a special "matrix" object that can cache its inverse.
+#
 makeCacheMatrix <- function(x = matrix()) {
+  inverse_matrix <- NULL
+  set <- function(y) {
+      x <<- y
+      inverse_matrix <<- NULL
+  }
+  get <- function() x
+  setinvmat <- function(inverse) inverse_matrix <<- inverse
+  getinvmat <- function() inverse_matrix 
+  list (set = set, get = get, setinvmat = setinvmat, getinvmat = getinvmat)
 
 }
 
 
 ## Write a short comment describing this function
+## cacheSolve take an input matrix x and determine whether or not the input matrix already
+##    defined its inverse . If inverse is not defined then use solve to calculate
+##    the inverse of x. If x's inverse matrix is already define, just grab it from 
+##    its cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  # test inverse matrix of x
+  #is the inverse matrix already defined
+  
+   invX <- x$getinvmat()
+   if (!is.null(invX)) {
+    message("Inverse matrix is already defined. Getting it from cache data")
+    return(invX)
+   }
+   else
+   {
+     message("Nothing in cache. Use solve to obtain the inverse of current matrix")
+     invX <- solve(x$get())
+     x$setinvmat(invX)
+     invX
+   }
 }
+####  test case
+#### define matrix  2x2 filling with index 1:4
+#> testmatrix <- makeCacheMatrix(rbind(1:2,3:4))
+#### Show it
+# > testmatrix$get()
+#[,1] [,2]
+#[1,]    1    2
+#[2,]    3    4
+#### Initialize with cacheSolve, thus caching its inverse
+#> cacheSolve(testmatrix)
+#Nothing in cache. Use solve to obtain the inverse of current matrix
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
+#### Second call with cacheSolve we will get it from its inverse
+#> cacheSolve(testmatrix)
+#Inverse matrix is already defined. Getting it from cache data
+#[,1] [,2]
+#[1,] -2.0  1.0
+#[2,]  1.5 -0.5
